59c28c279aea7143502ae4d796debcdbdd3bfda9
Roger Best <rbest@microsoft.com>
2022-08-23 12:40:56 -0700

    remove debug, change builder

--- builder/aci.py
+++ builder/aci.py
@@ -68,10 +68,10 @@ def main(names, params, suffix, skip_build=False):
                 else:
                     group_name = image['buildResourceGroup']
 
-                text_file = open(params_file,"r")
-                data = text_file.read()
-                text_file.close()
-                log.info(data)
+                #text_file = open(params_file,"r")
+                #data = text_file.read()
+                #text_file.close()
+                #log.info(data)
                 group = az.cli(['deployment', 'group', 'create', '-n', image['name'], '-g', group_name, '-f', bicep_file, '-p', params_file, '--no-prompt', '--subscription', image['subscription']])
 
     if skip_build:
