35ef402883a83eed43fe00bbaee58304725ac020
Roger Best <rbest@microsoft.com>
2022-11-02 14:09:49 -0700

    fix extension issues

--- scripts/Install-DockerDesktop.ps1
+++ scripts/Install-DockerDesktop.ps1
@@ -1,12 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-$env:chocolateyUseWindowsCompression = 'false'
+# $env:chocolateyUseWindowsCompression = 'false'
 
-Write-Host "Downloading Chocolatey ..."
-(new-object net.webclient).DownloadFile('https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe', 'C:\Windows\Temp\DockerDesktopInstaller.ps1')
+Write-Host "Downloading Docker Desktop ..."
+(new-object net.webclient).DownloadFile('https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe', 'C:\Windows\Temp\DockerDesktopInstaller.exe')
 
 Write-Host "Installing Docker Desktop ..."
-& C:/Windows/Temp/DockerDesktopInstaller.ps1
+& C:/Windows/Temp/DockerDesktopInstaller.exe
 
 Add-LocalGroupMember -Group "docker-users" -Member "Administrators"
